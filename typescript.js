18f0ab83b4962852c523fd4379ac7364fae27155
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-07-29 15:33:35 +0200

    feat : add a object in my project

---
+++ typescript.js
@@ -0,0 +1,11 @@
+let porfolio = {
+    nom: 'Dieme',
+    postNom: 'Md',
+    Adresse: {
+        quartier: 'kyeshero',
+        Av: 'Lusaka',
+        Num: 45
+    }
+}
+
+console.log(porfolio);
--- typescript.ts
+++ typescript.ts
@@ -1,12 +1,12 @@
-const input1 = document.querySelector('#input1')! as HTMLInputElement;
-const input2 = document.querySelector('#input2')! as HTMLInputElement;
-const btn = document.querySelector('button')
-
-const resultat = (number1 : number,number2 : number)=>{
-  
-    return number1 + number2; 
+const porfolio = {
+    nom : 'Dieme',
+    postNom : 'Md',
+    Adresse  : {
+        quartier : 'kyeshero',
+        Av : 'Lusaka',
+        Num : 45
+    }
 }
 
-btn?.addEventListener('click', ()=>{
-    console.log(resultat(+input1.value, +input2.value))
-})
+
+
