1d92a476942957a9618a7f3cf1b05b47af89c713
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-08-05 21:11:30 +0200

    feat : core type

--- typescript.js
+++ typescript.js
@@ -1,11 +1,14 @@
-let porfolio = {
+var porfolio = {
     nom: 'Dieme',
     postNom: 'Md',
     Adresse: {
         quartier: 'kyeshero',
         Av: 'Lusaka',
         Num: 45
-    }
+    },
+    hobbies: ['sport', 'sleeping', 'videoGame']
+};
+for (var _i = 0, _a = porfolio.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
 }
-
-console.log(porfolio);
--- typescript.ts
+++ typescript.ts
@@ -1,12 +1,21 @@
-const porfolio = {
+const porfolio : {
+    nom : string;
+    postNom : string;
+    Adresse : object;
+    hobbies : string[]
+} = {
     nom : 'Dieme',
     postNom : 'Md',
-    Adresse  : {
+    Adresse  :  {
         quartier : 'kyeshero',
         Av : 'Lusaka',
         Num : 45
-    }
+    },
+    hobbies : ['sport', 'sleeping','videoGame']
 }
 
+for(const hobby of porfolio.hobbies){
+    console.log(hobby)
+}
 
 
